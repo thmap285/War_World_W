e267fdaaf2dd570b6a93f61dbfed468fcb53d891
thmap285 <dinhthaimap285@gmail.com>
2025-03-04 11:38:51 +0700

    Update script
    Đổi tên class BulletController.cs thành BulletProjectile.cs

---
+++ Assets/Scripts/BulletProjectile.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private GameObject hitEnemyEffect;
+    // [SerializeField] private GameObject hitWallEffect;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    [System.Obsolete]
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        transform.rotation = Quaternion.LookRotation(transform.forward) * Quaternion.Euler(-90f,0,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Bullet") || other.gameObject.CompareTag("Player")) return;
+
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            var hitEffect = Instantiate(hitEnemyEffect, transform.position, 
+                            Camera.main.transform.rotation);
+            var hitEffectLifeTime = hitEffect.GetComponent<ParticleSystem>().main.duration;
+            
+            Destroy(hitEffect, hitEffectLifeTime);
+        }
+        // else if(other.gameObject.CompareTag("Wall"))
+        // {
+        //     var hitEffect = Instantiate(hitWallEffect, transform.position, Camera.main.transform.rotation);
+        //     var hitEffectLifeTime = hitEffect.GetComponent<ParticleSystem>().main.duration;
+
+        //     Destroy(hitEffect, hitEffectLifeTime);
+        // }
+
+        Destroy(gameObject);
+    }
+}
